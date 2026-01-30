6769838ebe964059e55b7c82490a6dfdd32d7c17
Jacob <kathund@proton.me>
2026-01-30 15:59:01 +0800

    feat: cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,13 @@ import svelte from '@astrojs/svelte';
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   outDir: './build',
   integrations: [svelte()],
   server: { host: true, allowedHosts: ['kathund.dev'], port: 44461 },
   vite: { plugins: [tailwindcss()] },
-  site: 'https://kathund.dev'
+  site: 'https://kathund.dev',
+  adapter: cloudflare()
 });
