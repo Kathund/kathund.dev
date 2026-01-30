2eafce60ac0e05b3dfa133aa05786f1652984c6f
Jacob <kathund@proton.me>
2026-01-30 15:22:21 +0800

    fix: astro cfg

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,6 @@ export default defineConfig({
   outDir: './build',
   integrations: [svelte()],
   server: { host: true, allowedHosts: ['kathund.dev'], port: 44461 },
-  vite: { plugins: [tailwindcss()] }
+  vite: { plugins: [tailwindcss()] },
+  site: 'https://kathund.dev'
 });
